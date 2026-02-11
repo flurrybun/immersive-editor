51de889344ec43f263983497001a7ad56a9455b5
flurrybun <122103556+flurrybun@users.noreply.github.com>
2026-02-11 12:37:19 -0500

    add support for custom enter effects

--- src/ShowFadeAndEnter.cpp
+++ src/ShowFadeAndEnter.cpp
@@ -1,83 +1,52 @@
-#include <Geode/modify/LevelEditorLayer.hpp>
-#include "misc/PlaytestEvent.hpp"
 #include "UpdateVisibility.hpp"
 
 #include <Geode/Geode.hpp>
 using namespace geode::prelude;
 
-class $modify(SFAELevelEditorLayer, LevelEditorLayer) {
-    // uncomment this whenever i work out the issue with enter effects
+bool shouldFadeObject(GameObject* object, bool isRight, short objectEnterType, int enterType) {
+    if (object->m_ignoreFade) return false;
+    if (object->m_isInvisibleBlock) return false;
+    if (enterType == -14 || enterType == -15) return false;
 
-    // struct Fields {
-    //     PlaytestEventListener playtestListener;
-    // };
+    // some blocks like the solid black blocks in clutterfunk don't fade
+    // this is the logic to handle that
 
-    // $override
-    // bool init(GJGameLevel* level, bool noUI) {
-    //     if (!LevelEditorLayer::init(level, noUI)) return false;
+    return (
+        !object->m_intrinsicDontFade ||
+        (object->m_isSolidColorBlock && object->m_baseOrDetailBlending) ||
+        objectEnterType > -1 ||
+        enterType != -2
+    );
+}
 
-    //     m_fields->playtestListener.bind([this](PlaytestEvent* event) {
-    //         if (event->isPlaying()) return ListenerResult::Propagate;
+void applyFadeEffect(LevelEditorLayer* lel, GameObject* object, bool isRight) {
+    CCPoint fadePos = object->getPosition();
 
-    //         for (const auto& object : CCArrayExt<GameObject*>(m_objects)) {
-    //             // resetting object scale is handled in AudioScale.cpp
-    //             object->setRRotation(0.f);
-    //         }
+    if (isRight) fadePos.x -= object->m_fadeMargin;
+    else fadePos.x += object->m_fadeMargin;
 
-    //         return ListenerResult::Propagate;
-    //     });
+    float distanceToEdge = isRight
+        ? lel->m_gameState.m_cameraPosition2.x + lel->m_cameraWidth - fadePos.x
+        : fadePos.x - lel->m_gameState.m_cameraPosition2.x;
 
-    //     return true;
-    // }
+    auto opacity = std::clamp(distanceToEdge / 70.f, 0.f, 1.f);
+    object->setOpacity(opacity * 255.f);
+}
 
-    bool shouldFadeObject(GameObject* object, bool isRight, short customEnterType, int enterType) {
-        if (object->m_ignoreFade) return false;
-        if (object->m_isInvisibleBlock) return false;
-        if (enterType == -14 || enterType == -15) return false;
+void applyEnterEffect(GameObject* object, bool isRight, int enterType) {
+    auto fakePL = reinterpret_cast<PlayLayer*>(GJBaseGameLayer::get());
 
-        // some blocks like the solid black blocks in clutterfunk don't fade
-        // this is the logic to handle that
+    CCPoint prevEEP = fakePL->m_enterEffectPosition;
+    fakePL->m_enterEffectPosition = object->getPosition();
 
-        return (
-            !object->m_intrinsicDontFade ||
-            (object->m_isSolidColorBlock && object->m_baseOrDetailBlending) ||
-            customEnterType > -1 ||
-            enterType != -2
-        );
+    if (enterType == -15) {
+        fakePL->applyCustomEnterEffect(object, isRight);
+    } else {
+        fakePL->applyEnterEffect(object, enterType, isRight);
     }
 
-    void applyFadeEffect(GameObject* object, bool isRight) {
-        CCPoint fadePos = object->getPosition();
-
-        if (isRight) fadePos.x -= object->m_fadeMargin;
-        else fadePos.x += object->m_fadeMargin;
-
-        float distanceToEdge = isRight
-            ? m_gameState.m_cameraPosition2.x + m_cameraWidth - fadePos.x
-            : fadePos.x - m_gameState.m_cameraPosition2.x;
-
-        auto opacity = std::clamp(distanceToEdge / 70.f, 0.f, 1.f);
-        object->setOpacity(opacity * 255.f);
-    }
-
-    void applyEnterEffect(GameObject* object, bool isRight, int enterType) {
-        auto fakePL = reinterpret_cast<PlayLayer*>(GJBaseGameLayer::get());
-
-        CCPoint prevEEP = fakePL->m_enterEffectPosition;
-        fakePL->m_enterEffectPosition = object->getPosition();
-
-        if (enterType == -15) {
-            // not fully working yet. for some reason custom enter effects
-            // only seem to work on the left half of the screen?
-
-            // fakePL->applyCustomEnterEffect(object, isRight);
-        } else {
-            fakePL->applyEnterEffect(object, enterType, isRight);
-        }
-
-        fakePL->m_enterEffectPosition = prevEEP;
-    }
-};
+    fakePL->m_enterEffectPosition = prevEEP;
+}
 
 float ie::preUpdateFadeAndEnter(LevelEditorLayer* lel) {
     return lel->m_gameState.m_cameraPosition2.x + lel->m_cameraWidth / 2;
@@ -90,7 +59,7 @@ void ie::updateFadeAndEnter(LevelEditorLayer* lel, GameObject* object, float cam
     bool isRight = object->getPositionX() > cameraXCenter;
     bool isEnter = isRight || object->m_enterType != -1;
 
-    short customEnterType = isEnter
+    short objectEnterType = isEnter
         ? object->m_enterType
         : object->m_exitType;
 
@@ -98,13 +67,25 @@ void ie::updateFadeAndEnter(LevelEditorLayer* lel, GameObject* object, float cam
         ? lel->m_gameState.m_enterChannelMap[object->m_enterChannel]
         : lel->m_gameState.m_exitChannelMap[object->m_enterChannel];
 
-    auto modLel = static_cast<SFAELevelEditorLayer*>(lel);
+#if GEODE_COMP_GD_VERSION == 22074
+    float* cameraRight = reinterpret_cast<float*>(geode::base::get() +
+        GEODE_WINDOWS(0x6a304c)
+        GEODE_INTEL_MAC(0x000000)
+        GEODE_ARM_MAC(0x000000)
+        GEODE_ANDROID64(0x000000)
+        GEODE_ANDROID32(0x000000)
+        GEODE_IOS(0x000000)
+    );
+    *cameraRight = lel->m_gameState.m_cameraPosition2.x + lel->m_cameraWidth;
+#else
+    #error "Incorrect GD version: no address for cameraRight global"
+#endif
 
-    if (modLel->shouldFadeObject(object, isRight, customEnterType, enterType)) {
-        modLel->applyFadeEffect(object, isRight);
+    if (shouldFadeObject(object, isRight, objectEnterType, enterType)) {
+        applyFadeEffect(lel, object, isRight);
     }
 
     if (!object->m_isDontEnter) {
-        modLel->applyEnterEffect(object, isRight, enterType);
+        applyEnterEffect(object, isRight, enterType);
     }
 }
